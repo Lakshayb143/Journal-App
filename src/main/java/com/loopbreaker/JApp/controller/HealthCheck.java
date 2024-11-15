7bfc341289ed1e243e2e4b6f9ccb74763012abd3
Lakshay Bhatia <coderlakshay289143@gmail.com>
2024-11-15 20:15:34 +0530

    added html for information

--- src/main/java/com/loopbreaker/JApp/controller/HealthCheck.java
+++ src/main/java/com/loopbreaker/JApp/controller/HealthCheck.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheck {
 
+
+    @GetMapping
+    public String welcome(){
+        return "<h1>Welcome to My API</h1>";
+    }
+
     @GetMapping("/health-check")
     public String healthCheck(){
         return "Everything is working ok.";
